77854ffc13b16f14bfa4ad093e92637724a87ee0
aleksandresher <aleksandre.shervashidze.2@iliauni.edu.ge>
2023-06-08 16:44:48 +0400

    up

--- routes/auth.js
+++ routes/auth.js
@@ -3,6 +3,7 @@ const { body } = require("express-validator/check");
 
 const User = require("../models/user");
 const authController = require("../controllers/auth");
+const isAuth = require("../middleware/is-auth");
 
 const router = express.Router();
 
@@ -32,6 +33,6 @@ router.put(
   authController.signup
 );
 
-router.post("/", authController.login);
+router.post("/", isAuth, authController.login);
 
 module.exports = router;
