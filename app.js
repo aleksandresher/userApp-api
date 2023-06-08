eee6415f291a12e3e9dfda9cb89891be9825c584
aleksandresher <aleksandre.shervashidze.2@iliauni.edu.ge>
2023-06-08 15:29:12 +0400

    Update app.js

--- app.js
+++ app.js
@@ -18,6 +18,7 @@ app.use((req, res, next) => {
   res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
   next();
 });
+///sds
 
 app.use("/users", usersRoutes);
 app.use("/", authRoutes);
