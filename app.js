8c7c7c3fdce4bf1c10800d5122380ab2032009b1
aleksandresher <aleksandre.shervashidze.2@iliauni.edu.ge>
2023-06-08 15:44:40 +0400

    up

--- app.js
+++ app.js
@@ -26,7 +26,7 @@ app.use("/", authRoutes);
 mongoose
   .connect(process.env.DATABASE_URL)
   .then((result) => {
-    app.listen(8080);
+    app.listen(process.env.DATABASE_URL);
     console.log("server is running");
   })
   .catch((err) => console.log(err));
