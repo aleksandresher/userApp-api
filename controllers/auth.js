30a4f90b9a5bd10894de096b3da0527ed2094ccf
aleksandresher <89658328+aleksandresher@users.noreply.github.com>
2023-06-09 21:47:29 +0400

    Update auth.js

--- controllers/auth.js
+++ controllers/auth.js
@@ -36,55 +36,108 @@ exports.signup = (req, res, next) => {
     });
 };
 
-exports.login = (req, res, next) => {
+// exports.login = (req, res, next) => {
+//   const email = req.body.email;
+//   const password = req.body.password;
+//   let loadedUser;
+//   User.findOne({ email: email })
+//     .then((user) => {
+//       if (!user) {
+//         const error = new Error("A user with this email could not be found.");
+//         error.statusCode = 401;
+//         throw error;
+//       }
+
+//       if (!user.active) {
+//         const error = new Error("Your account is currently inactive.");
+//         error.statusCode = 401;
+//         throw error;
+//       }
+//       loadedUser = user;
+//       return bcrypt.compare(password, user.password);
+//     })
+//     .then((isEqual) => {
+//       if (!isEqual) {
+//         const error = new Error("Wrong password!");
+//         error.statusCode = 401;
+//         throw error;
+//       }
+
+//       loadedUser.lastLogin = new Date();
+//       return loadedUser.save();
+//     })
+//     .then(() => {
+//       const token = jwt.sign(
+//         {
+//           email: loadedUser.email,
+//           userId: loadedUser._id.toString(),
+//         },
+//         "secretkey",
+//         { expiresIn: "1h" }
+//       );
+//       res.status(200).json({
+//         token: token,
+//         userId: loadedUser._id.toString(),
+//         message: "welcome",
+//       }); /// neeed to take this on react
+//     })
+//     .catch((err) => {
+//       if (!err.statusCode) {
+//         err.statusCode = 500;
+//       }
+//       next(err);
+//     });
+// };
+
+exports.login = async (req, res, next) => {
   const email = req.body.email;
   const password = req.body.password;
-  let loadedUser;
-  User.findOne({ email: email })
-    .then((user) => {
-      if (!user) {
-        const error = new Error("A user with this email could not be found.");
-        error.statusCode = 401;
-        throw error;
-      }
 
-      if (!user.active) {
-        const error = new Error("Your account is currently inactive.");
-        error.statusCode = 401;
-        throw error;
-      }
-      loadedUser = user;
-      return bcrypt.compare(password, user.password);
-    })
-    .then((isEqual) => {
-      if (!isEqual) {
-        const error = new Error("Wrong password!");
-        error.statusCode = 401;
-        throw error;
-      }
+  try {
+    const user = await User.findOne({ email: email });
 
-      loadedUser.lastLogin = new Date();
-      return loadedUser.save();
-    })
-    .then(() => {
-      const token = jwt.sign(
-        {
-          email: loadedUser.email,
-          userId: loadedUser._id.toString(),
-        },
-        "secretkey",
-        { expiresIn: "1h" }
-      );
-      res.status(200).json({
-        token: token,
-        userId: loadedUser._id.toString(),
-        message: "welcome",
-      }); /// neeed to take this on react
-    })
-    .catch((err) => {
-      if (!err.statusCode) {
-        err.statusCode = 500;
-      }
-      next(err);
+    if (!user) {
+      const error = new Error("A user with this email could not be found.");
+      error.statusCode = 401;
+      throw error;
+    }
+
+    if (!user.active) {
+      const error = new Error("Your account is currently inactive.");
+      error.statusCode = 401;
+      throw error;
+    }
+
+    const isEqual = await bcrypt.compare(password, user.password);
+
+    if (!isEqual) {
+      const error = new Error("Wrong password!");
+      error.statusCode = 401;
+      throw error;
+    }
+
+    user.lastLogin = new Date();
+    await user.save();
+
+    const token = jwt.sign(
+      {
+        email: user.email,
+        userId: user._id.toString(),
+      },
+      "secretkey",
+      { expiresIn: "1h" }
+    );
+
+    res.status(200).json({
+      token: token,
+      userId: user._id.toString(),
+      message: "welcome",
     });
+  } catch (err) {
+    if (!err.statusCode) {
+      err.statusCode = 500;
+    }
+    next(err);
+  }
 };
+
